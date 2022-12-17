2652592935234decdc7dd994d009a54e176fbafb
Arathy s s <112745111+arathyy@users.noreply.github.com>
2022-12-17 11:09:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import './App.css'
+import{useState} from 'react'
+function App(){
+  const [toDos,setTodos]=useState([])
+  const [toDo,setToDo]=useState(' ')
+  
+ 
+ return (
+  
+      <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1> 
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input  value={toDo} onChange={(e)=>setToDo(e.target.value)}type="text" placeholder="🖊️ Add item..." />
+
+        <i onClick={()=>setTodos([...toDos,{id:Date.now() ,text:toDo,status:false}])} className="fas fa-plus"></i>
+        </div>
+      <div className="todos">
+        { toDos.map((obj)=>{
+
+        return (<div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(obj)
+              setTodos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked 
+                }
+                return obj2
+              }))
+            }} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+            
+          </div>
+          <div className="right">
+            <i id={obj.id} className="fas fa-times" onClick={(e)=>{
+                let index= toDos.findIndex(obj=>{return obj.id==e.target.id})
+                if (index !== -1) {
+                  toDos.splice(index, 1);
+                  setTodos([...toDos]);
+                }
+              }}>
+            </i>
+          </div>
+        </div>)
+        }) }
+        {toDos.map((obj)=>{
+          if(obj.staus){
+            return(<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
+      </div>
+    </div>
+  );
+  
+}
+export default App;
+
+
+ 
